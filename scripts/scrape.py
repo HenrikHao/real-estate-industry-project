1dbbfec732324b87aefb9afcfacbc63abcd3fc6a
HenrikHao <73416544+HenrikHao@users.noreply.github.com>
2023-09-03 22:52:00 +1000

    update scrape strategy

--- scripts/scrape.py
+++ scripts/scrape.py
@@ -1,104 +1,103 @@
-"""
-A very simple, naive, and basic web scraping script. This will not
-work completely and is only applicable to domain.com.
-
-Feel free to use this as a source of inspiration, it is by no means production code.
-"""
-# built-in imports
 import re
+import time
 from json import dump
-
 from collections import defaultdict
-
-# user packages
 from bs4 import BeautifulSoup
-from urllib.request import urlopen
+import requests
 
 # constants
 BASE_URL = "https://www.domain.com.au"
-N_PAGES = range(1, 5) # update this to your liking
+MAX_PAGES = 50
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36"
+}
 
-# begin code
 url_links = []
 property_metadata = defaultdict(dict)
 
-# generate list of urls to visit
-for page in N_PAGES:
-    url = BASE_URL + f"/rent/melbourne-region-vic/?sort=price-desc&page={page}"
-    print(f"Visiting {url}")
-    bs_object = BeautifulSoup(urlopen(url), "lxml")
+def make_request(url):
+    try:
+        response = requests.get(url, headers=HEADERS, timeout=60)
+        return BeautifulSoup(response.text, "lxml")
+    except Exception as e:
+        print(f"Error while fetching {url}. Error: {e}")
+        return None
 
-    # find the unordered list (ul) elements which are the results, then
-    # find all href (a) tags that are from the base_url website.
-    index_links = bs_object \
-        .find(
-            "ul",
-            {"data-testid": "results"}
-        ) \
-        .findAll(
-            "a",
-            href=re.compile(f"{BASE_URL}/*") # the `*` denotes wildcard any
-        )
+for postcode in range(3000, 4000):
+    for page in range(1, MAX_PAGES + 1):
+        url = BASE_URL + f"/rent/?excludedeposittaken=1&postcode={postcode}&page={page}"
+        print(f"Visiting {url}")
+        
+        bs_object = make_request(url)
+        
+        if not bs_object:
+            print(f"Error accessing {url}. Skipping...")
+            continue
 
-    for link in index_links:
-        # if its a property address, add it to the list
-        if 'address' in link['class']:
-            url_links.append(link['href'])
+        try:
+            index_links = bs_object \
+                .find("ul", {"data-testid": "results"}) \
+                .findAll("a", href=re.compile(f"{BASE_URL}/*"))
+            
+            if not index_links:
+                print(f"No listings found on page {page} for postcode {postcode}.")
+                break
 
+            # Extracting property links...
+            for link in index_links:
+                if 'address' in link['class']:
+                    url_links.append(link['href'])
 
-# for each url, scrape some basic metadata
-for property_url in url_links[1:]:
-    bs_object = BeautifulSoup(urlopen(property_url), "lxml")
-    
-    try: 
-        # looks for the header class to get property name
+            time.sleep(5)  # Sleep for 5 seconds between page requests to avoid rate limits
+
+        except AttributeError as e:
+            print(f"Unexpected page structure at {url}. Error: {e}")
+            break
+
+for property_url in url_links:
+    bs_object = make_request(property_url)
+    if not bs_object:
+        continue
+
+    try:
         property_metadata[property_url]['name'] = bs_object \
             .find("h1", {"class": "css-164r41r"}) \
             .text
 
-        # looks for the div containing a summary title for cost
         property_metadata[property_url]['cost_text'] = bs_object \
             .find("div", {"data-testid": "listing-details__summary-title"}) \
             .text
 
-        # extract coordinates from the hyperlink provided
-        # i'll let you figure out what this does :P
         property_metadata[property_url]['coordinates'] = [
             float(coord) for coord in re.findall(
-                r'destination=([-\s,\d\.]+)', # use regex101.com here if you need to
+                r'destination=([-\s,\d\.]+)',
                 bs_object \
                     .find(
                         "a",
-                        {"target": "_blank", 'rel': "noopener noreferer"}
+                        {"target": "_blank", 'rel': "noopener noreferrer"}
                     ) \
                     .attrs['href']
             )[0].split(',')
         ]
 
-        # get rooms and parking
-        rooms = bs_object \
+        rooms_list = []
+        for feature in bs_object \
                 .find("div", {"data-testid": "property-features"}) \
-                .findAll("span", {"data-testid": "property-features-text-container"})
+                .findAll("span", {"data-testid": "property-features-text-container"}):
+            try:
+                rooms_list.append(re.findall(r'\d\s[A-Za-z]+', feature.text)[0])
+            except IndexError:
+                pass
 
-        # rooms
-        property_metadata[property_url]['rooms'] = [
-            re.findall(r'\d+\s[A-Za-z]+', feature.text)[0] for feature in rooms
-            if 'Bed' in feature.text or 'Bath' in feature.text
-        ]
-        # parking
-        property_metadata[property_url]['parking'] = [
-            re.findall(r'\S+\s[A-Za-z]+', feature.text)[0] for feature in rooms
-            if 'Parking' in feature.text
-        ]
+        property_metadata[property_url]['rooms'] = rooms_list
+        property_metadata[property_url]['desc'] = bs_object.find("p").text
 
-        property_metadata[property_url]['desc'] = re \
-            .sub(r'<br\/>', '\n', str(bs_object.find("p"))) \
-            .strip('</p>')
     except AttributeError:
-        print(f"Issue with {url}, see raw dump below")
+        print(f"Issue with {property_url}, see raw dump below")
         print(bs_object.text)
         continue
 
-# output to example json in data/raw/
-with open('../data/raw/example.json', 'w') as f:
+    time.sleep(5)  # Sleep for 5 seconds between individual property requests
+
+with open('../data/raw/property.json', 'w') as f:
     dump(property_metadata, f)
